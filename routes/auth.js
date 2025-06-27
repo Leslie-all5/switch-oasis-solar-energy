fbb43fcc928b6a7f745dae2d913bc9c39f05d949
Leslie-all5 <snasphantom@gmail.com>
2025-06-27 03:16:37 +0000

    Added dashboard.html page

--- routes/auth.js
+++ routes/auth.js
@@ -30,7 +30,7 @@ router.post('/register', async (req, res) => {
 
   } catch (err) {
     console.error(err.message);
-    res.status(500).json({ error: 'Server error' });
+    res.status(500).json({ error: 'Email already exits'});
   }
 });
 // LOGIN ROUTE
